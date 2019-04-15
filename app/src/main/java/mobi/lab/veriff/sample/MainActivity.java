abb9de2a066890cf9c4c722482783a2fe1f8df49
Vivekanandan Ani Sasikala <vivekanandan.sasikala@vernot056.local>
2019-04-15 18:08:53 +0300

    fixed header color schema and camera surface view crash

--- app/src/main/java/mobi/lab/veriff/sample/MainActivity.java
+++ app/src/main/java/mobi/lab/veriff/sample/MainActivity.java
@@ -1,6 +1,7 @@
 package mobi.lab.veriff.sample;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -18,6 +19,7 @@ import java.net.URL;
 import java.security.MessageDigest;
 import java.util.Date;
 
+import mobi.lab.veriff.data.ColorSchema;
 import mobi.lab.veriff.data.Veriff;
 import mobi.lab.veriff.network.AcceptHeaderInterceptor;
 import mobi.lab.veriff.sample.data.TokenPayload;
@@ -140,9 +142,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void launchVeriffSDK(@NonNull String sessionToken) {
+        final ColorSchema schema = new ColorSchema.Builder()
+                .setHeader(Color.TRANSPARENT, 1)
+                .setBackground(Color.DKGRAY)
+                .setFooter(Color.GRAY, 1)
+                //.setControlsColor(ContextCompat.getColor(this, R.color.red))
+                .build();
+
         //enable logging for the library
         Veriff.setLoggingImplementation(Log.getInstance(MainActivity.class));
         Veriff.Builder veriffSDK = new Veriff.Builder(baseUrl, sessionToken);
+        veriffSDK.setCustomColorSchema(schema);
+
         veriffSDK.launch(MainActivity.this, REQUEST_VERIFF);
     }
 
