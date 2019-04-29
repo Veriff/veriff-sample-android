d51d629c7dd8b0cdf3b64d5a01dfc8f1bb239ada
Vivekanandan Ani Sasikala <vivekanandan.sasikala@vernot056.local>
2019-04-29 16:20:03 +0300

    SDK-212 WIP added dummy login screen

--- app/src/main/java/com/veriff/demo/login/LoginActivity.kt
+++ app/src/main/java/com/veriff/demo/login/LoginActivity.kt
@@ -1,13 +1,31 @@
 package com.veriff.demo.login
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.veriff.demo.MainActivity
 import com.veriff.demo.R
+import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), LoginMVP.View {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener {
+            startMainFlow()
+        }
+
+
+        txtSkipLogin.setOnClickListener {
+            startMainFlow()
+        }
     }
+
+    override fun startMainFlow() {
+        MainActivity.start(this)
+        finish()
+    }
+
+
 }
---
+++ app/src/main/java/com/veriff/demo/login/LoginMVP.kt
@@ -0,0 +1,12 @@
+package com.veriff.demo.login
+
+interface LoginMVP {
+
+
+    interface View {
+        fun startMainFlow()
+    }
+
+
+    interface Presenter
+}
