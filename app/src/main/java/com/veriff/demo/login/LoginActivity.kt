0cee5500ac6efe369f202c025e640f2cdb2d60dc
Vivekanandan Ani Sasikala <vivekanandan.sasikala@vernot056.local>
2019-04-29 15:56:54 +0300

    Enabled Kotlin

---
+++ app/src/main/java/com/veriff/demo/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.veriff.demo.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.veriff.demo.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
