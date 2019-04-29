969dfd4c1c2c1d0ca474141930c2df06c28c7785
Madis Pink <madis.pink@gmail.com>
2019-04-29 06:32:43 +0300

    Change appid to com.veriff.demo

--- app/src/androidTest/java/mobi/lab/veriff/sample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/mobi/lab/veriff/sample/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("mobi.lab.veriff.sample", appContext.getPackageName());
+        assertEquals("com.veriff.demo", appContext.getPackageName());
     }
 }
