4f36cc927aa16d0f5eb830c08cbff506a7cd703f
Madis Pink <mail@madis.pink>
2019-05-23 14:00:04 +0300

    SDK-294 Don't include country & doc in Trial app token
    Also default to 'en' language instead of 'et'.

--- app/src/main/java/com/veriff/demo/data/TokenPayload.java
+++ app/src/main/java/com/veriff/demo/data/TokenPayload.java
@@ -20,12 +20,12 @@ public class TokenPayload {
 
     public static class Verification {
         private Document document;
-        private HashMap additionalData = new HashMap();
+        private HashMap additionalData;
         private Date timestamp = new Date();
         private String lang = "en";
-        private String[] features = new String[]{"selfId"};
+        private String[] features = new String[]{"selfid"};
         private Person person;
-        private String callback = "https://awesome.page";
+        private String callback;
 
         public Document getDocument() {
             return document;
