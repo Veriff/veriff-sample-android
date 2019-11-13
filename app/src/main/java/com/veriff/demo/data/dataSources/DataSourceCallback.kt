168cde4430a0e1e20a2589d276b1f9e301f897d5
Madis Pink <mail@madis.pink>
2019-11-13 12:13:43 +0200

    SDK-854 Deprecate veriff-sample-android repository

--- app/src/main/java/com/veriff/demo/data/dataSources/DataSourceCallback.kt
+++
@@ -1,6 +0,0 @@
-package com.veriff.demo.data.dataSources
-
-interface DataSourceCallback {
-    fun gotData(data: Any?)
-    fun error(msg: String)
-}
--- app/src/main/java/com/veriff/demo/utils/qrCodeParser/QrCodeContentsParser.kt
+++
@@ -1,5 +0,0 @@
-package com.veriff.demo.utils.qrCodeParser
-
-interface QrCodeContentsParser {
-    fun parseQrCodeContents(contents: String): Pair<String?, String?>
-}
